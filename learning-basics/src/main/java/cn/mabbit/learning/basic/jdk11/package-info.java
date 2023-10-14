ad207fece3170d70deb2f39909b8c3f96a31db4f
schuke <xk233qwq@163.com>
2023-10-14 17:22:23 +0800

    2023/10/14

---
+++ learning-basics/src/main/java/cn/mabbit/learning/basic/jdk11/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <h2>jdk9-11学习包</h2>
+ */
+package cn.mabbit.learning.basic.jdk11;
---
+++ learning-basics/src/main/java/cn/mabbit/learning/basic/jdk8/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <h2>jdk8及以下版本学习包</h2>
+ *
+ * <p>package-info 是特殊的Java源文件，用于给当前包添加注释</p>
+ */
+package cn.mabbit.learning.basic.jdk8; // package关键字用于指定该源文件所在包
